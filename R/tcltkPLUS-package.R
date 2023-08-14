b08ca18aaa53a3fd99031914746e42cad85addf1
Jake1Egelberg <jake1egelberg@gmail.com>
2023-08-14 15:40:34 -0400

    first commit

---
+++ R/tcltkPLUS-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import tcltk
+#' @import tkrplot
+#' @import stringr
+#' @import this.path
+#' @import ggplot2
+#' @importFrom utils write.table
+#' @importFrom methods is
+## usethis namespace: end
+NULL
---
+++ tests/testthat/test-tcSubmissionbox.R
@@ -0,0 +1,16 @@
+library(tcltkPLUS)
+library(testthat)
+
+tcTheme("DEFAULT")
+
+#Run test
+test_that("Check tcBuild",{
+  expect_no_error(
+    tcSubmissionbox("test","test","test",self_destruct = TRUE)
+  )
+  expect_visible(
+    tcSubmissionbox("test","test","test",self_destruct = TRUE)
+  )
+})
+
+
